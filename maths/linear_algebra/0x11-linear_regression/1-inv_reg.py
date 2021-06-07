7d9c93da03572749eab1bc1fcbbb0299c990f942
root <DennisWanjeri>
2021-06-07 12:57:59 +0000

    inverse regression

---
+++ maths/linear_algebra/0x11-linear_regression/1-inv_reg.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from numpy.linalg import inv
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pyplot
+
+data = np.array([
+    [0.05, 0.12],
+    [0.18, 0.22],
+    [0.31, 0.35],
+    [0.42, 0.38],
+    [0.5, 0.49]
+])
+#split into inputs and outputs
+x, y = data[:, 0], data[:, 1]
+x = x.reshape((len(x), 1))
+#linear least squares
+b = inv(x.T.dot(x)).dot(x.T).dot(y)
+print(b)
+#predict using coefficients
+yhat = x.dot(b)
+#plot data and predictions
+pyplot.scatter(x, y)
+pyplot.plot(x, yhat, color='red')
+pyplot.savefig('1-inv.png')
