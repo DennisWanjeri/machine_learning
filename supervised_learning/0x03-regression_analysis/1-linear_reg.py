57393a470326df349c855db47659d526c404c3d9
root <DennisWanjeri>
2021-06-20 08:29:35 +0000

    mean annual

---
+++ supervised_learning/0x03-regression_analysis/1-linear_reg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Linear regression"""
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('synth_temp.csv')
+df = df.loc[df.Year > 1901]
+df_group_year = df.groupby('Year').agg(np.mean)
+window = 10
+rolling = df_group_year.AverageTemperature.rolling(window).mean()
+print(rolling.head(n=20))
+#plotting measurements by year along with mobing average signal
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_axes([1, 1, 1, 1])
+#Temp measurements
+ax.scatter(df_group_year.index, df_group_year.AverageTemperature, label='Raw Data', c='k')
+ax.plot(df_group_year.index, rolling, c='k', linestyle='--',
+        label='{} year moving average'.format(window))
+ax.set_title('Mean Air Temperature Measurements')
+ax.set_xlabel('Year')
+ax.set_ylabel('Temperature (degC)')
+ax.set_xticks(range(df_group_year.index.min(), df_group_year.index.max(), 10))
+ax.legend();
+plt.savefig('mean_annual_temp.png')
