2a3c34954b4df83b64e9ef33ed86b6c82d2eb67b
root <DennisWanjeri>
2021-06-20 18:24:48 +0000

    first linear model

--- supervised_learning/0x03-regression_analysis/1-linear_reg.py
+++ supervised_learning/0x03-regression_analysis/1-linear_reg.py
@@ -5,6 +5,7 @@ matplotlib.use('Agg')
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('synth_temp.csv')
 df = df.loc[df.Year > 1901]
@@ -12,12 +13,29 @@ df_group_year = df.groupby('Year').agg(np.mean)
 window = 10
 rolling = df_group_year.AverageTemperature.rolling(window).mean()
 print(rolling.head(n=20))
-#plotting measurements by year along with mobing average signal
+
+#Fitting a linear model using the Least Squares Method
+model = LinearRegression()
+print(model)
+model.fit(df_group_year.index.values.reshape((-1, 1)), df_group_year.AverageTemperature)
+print("m = {}".format(model.coef_[0]))
+print("c = {}".format(model.intercept_))
+print('\nModel Definition')
+print('y = {}x + {}'.format(model.coef_[0], model.intercept_))
+trend_x = np.array([
+    df_group_year.index.values.min(),
+    df_group_year.index.values.mean(),
+    df_group_year.index.values.max()
+])
+trend_y = model.predict(trend_x.reshape((-1, 1)))
+print(trend_y)
+#plotting measurements by year along with moving average signal
 plt.figure(figsize=(10, 7))
 #Temp measurements
 plt.scatter(df_group_year.index, df_group_year.AverageTemperature, label='Raw Data', c='k')
 plt.plot(df_group_year.index, rolling, c='k', linestyle='--',
-        label='{} year moving average'.format(window))
+         label='{} year moving average'.format(window))
+plt.plot(trend_x, trend_y, c='k', label='Model: Predicted trendline')
 plt.title('Mean Air Temperature Measurements')
 plt.xlabel('Year')
 plt.ylabel('Temperature (degC)')
