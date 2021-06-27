6a2f424bfc9087ac17ac0e6dd0f4526666e3970b
DennisWanjeri <shikandadennis07@gmail.com>
2021-06-27 08:51:58 +0000

    autoreg

--- supervised_learning/0x03-regression_analysis/6-autoregression.py
+++ supervised_learning/0x03-regression_analysis/6-autoregression.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from statsmodels.tsa.ar_model import AR
 
 df = pd.read_csv('spx.csv')
 print(df.head())
@@ -26,10 +27,16 @@ print(df.head(n=20))
 
 print(df.close[:10].values)
 print(df.close[:10].shift(3).values)
+model = AR(df.close)
+model_fit = model.fit()
+print("Lag: {}".format(model_fit.k_ar))
+print("coefficients: {}".format(model_fit.params))
+predictions = model_fit.predict(start=36, end=len(df) + 500)
+print(predictions[:10].values)
 #plotting
 plt.figure(figsize=(10, 7))
-plt.plot(df.close.values)
-plt.plot(df.close.shift(100), c='k', linestyle=':', label='Lag 100')
+plt.plot(df.close.values, label='original dataset')
+plt.plot(predictions, c='g', linestyle=':', label='predictions')
 yrs = [yr for yr in df.Year.unique() if (int(yr[-2:]) % 5 == 0)]
 plt.xticks(np.arange(0, len(df), len(df) // len(yrs)), yrs)
 plt.title('S&P 500 Daily Closing Price')
@@ -48,5 +55,5 @@ ax.set_ylim([-0.1, 0.1])
 plt.savefig('visualized.png')
 #autocorrelation plot
 plt.figure(figsize=(10,7))
-pd.plotting.lag_plot(df.close, lag=100)
+pd.plotting.lag_plot(df.close, lag=4000)
 plt.savefig('lag_plot.png')
